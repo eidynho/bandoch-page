83e9525ece7ce837c00a2f0de30bbb225d89efa0
Vinicius Eidy <zviniciuseidy2@gmail.com>
2022-08-21 19:00:10 -0300

    New: adding base section component

---
+++ src/components/Section.vue
@@ -0,0 +1,16 @@
+<script>
+
+export default {
+  data() {
+    return {
+
+}
+  },
+}
+</script>
+
+<template>
+  <section class="h-[100vh] z-10 flex flex-col lg:flex-row justify-center bg-[#EEEAEA]">
+
+  </section>
+</template>
--- src/index.vue
+++ src/index.vue
@@ -2,42 +2,50 @@
 import gsap from 'gsap'
 import ScrollTrigger from 'gsap/ScrollTrigger'
 
+import Welcome from './components/Welcome.vue'
 import Navbar from './components/Navbar.vue'
 import Header from './components/Header.vue'
 import Main from './components/Main.vue'
+import Section from './components/Section.vue'
 import Footer from './components/Footer.vue'
 
 export default {
   components: {
+    Welcome,
     Navbar,
     Header,
     Main,
+    Section,
     Footer,
   },
   data() {
     return {
+
     }
   },
   mounted() {
     gsap.registerPlugin(ScrollTrigger)
-     gsap.to('#footerComponent', {
-        opacity: 0.9,
-        scrollTrigger: {
-          trigger: '#mainComponent',
-          start: 'bottom bottom',
-          end: 'bottom top',
-          scrub: true,
-        }
-      })
+    gsap.to('#footerComponent', {
+      opacity: 0.9,
+      scrollTrigger: {
+        trigger: '#sectionComponent',
+        start: 'bottom bottom',
+        end: 'bottom top',
+        scrub: true,
+      }
+    })
+
   }
 }
 </script>
 
 <template>
-  <div class="flex flex-col w-full mx-auto max-w-[1340px]">
+  <div class="flex flex-col w-full">
+    <!-- <Welcome /> -->
     <Navbar />
     <Header />
-    <Main id="mainComponent" />
+    <Main />
+    <Section id="sectionComponent" />
   </div>
   <div class="w-full h-[100vh] bg-[#EEEAEA]">
     <Footer id="footerComponent" style="opacity: 0;" />
