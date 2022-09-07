b71f2e034a545a60c5d3bb86577f8b5b5553b9d7
Vinicius Eidy <zviniciuseidy2@gmail.com>
2022-09-06 22:16:46 -0300

    New: CardDetails component

--- src/components/CardDetails.vue
+++ src/components/CardDetails.vue
@@ -1,33 +1,136 @@
 <script>
 import { useRoute } from 'vue-router'
 
+import cardHxh from '../assets/card-hxh.jpg'
+import cardEidy from '../assets/card-eidy.jpg'
+import cardAriana from '../assets/card-ariana.jpg'
+import cardRobots from '../assets/card-robots.jpg'
+
   export default {
-    props: {
-      id: String,
-      img: String,
-      imgAlt: String,
-      title: String,
-      tags: String,
-      isWhite: String,
-    },
     data() {
       return {
+        cardHxh,
+        cardEidy,
+        cardAriana,
+        cardRobots,
+
         cardName: '',
+        title: '',
+        type: '',
+        year: 2022,
+        country: '',
+        cardImage: undefined,
+        imageAlt: '',
       }
     },
     mounted() {
       const route = useRoute()
       this.cardName = route.params.name
+
+      this.getData(this.cardName)
+    },
+    methods: {
+      getData(cardName) {
+        switch (cardName) {
+          case "hxh":
+            this.title = "HxH"
+            this.type = "Design gráfico"
+            this.year = 2022
+            this.country = 'Brasil'
+            this.cardImage = cardHxh
+            this.imageAlt = 'Card Hunter x Hunter'
+            break
+          case "planet":
+            this.title = "HxH"
+            this.type = "Design gráfico"
+            this.year = 2022
+            this.country = 'Brasil'
+            this.cardImage = cardEidy
+            this.imageAlt = 'Card Planet'
+            break
+          case "ariana":
+            this.title = "Ariana Grande"
+            this.type = "Design gráfico"
+            this.year = 2022
+            this.country = 'Brasil'
+            this.cardImage = cardAriana
+            this.imageAlt = 'Card Ariana Grande'
+            break
+          case "robots":
+            this.title = "Robots"
+            this.type = "Design gráfico"
+            this.year = 2022
+            this.country = 'Brasil'
+            this.cardImage = cardRobots
+            this.imageAlt = 'Card Robots'
+            break
+        }
+      },
     }
   }
-  </script>
+</script>
   
-  <template>
-    <div>
-      teste, {{ cardName }}
+<template>
+  <div class="max-w-[1340px] mx-auto flex flex-col items-center justify-center">
+    <h1 class="text-8xl mt-40 mb-20">{{ title }}</h1>
+
+    <div class="w-1/2 flex justify-between">
+      <div>
+        <h3>Tipo</h3>
+        <span>{{ type }}</span>
+      </div>
+      <div>
+        <h3>Ano</h3>
+        <span>{{ year }}</span>
+      </div>
+      <div>
+        <h3>País</h3>
+        <span>{{ country }}</span>
+      </div>
     </div>
-  </template>
+
+    <div class="z-10 relative mt-36 rounded-3xl max-w-3xl mx-auto cardDimensions">
+      <img :src="cardImage" :alt="imageAlt" class="w-full h-full rounded-3xl">
+    </div>
+
+    <footer class="mt-40 w-full flex justify-between">
+      <div class="flex flex-col">
+        <h6>Redes sociais</h6>
+        <ul>
+          <li>Instagram</li>
+          <li>Behance</li>
+        </ul>
+      </div>
+
+      <div>
+        <span>© MIGUEL BANDOCH, TODOS OS DIREITOS RESERVADOS</span>
+      </div>
+
+      <div>
+        <h6>E-mail</h6>
+        <span>email@gmail.com</span>
+
+        <h6>Link</h6>
+        <a href="" class="inline">{{ cardName }}</a>
+      </div>
+    </footer>
+
+  </div>
+</template>
   
   
-  <style scoped lang="scss">
-  </style>
+<style scoped lang="scss">
+.cardDimensions {
+  width: 100%;
+
+  @media(min-width: 1024px) {
+    width: 460px;
+    height: 651px;
+  }
+
+  @media(min-width: 1380px) {
+    width: 630px;
+    height: 891px;
+  }
+}
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,63 +1,74 @@
 <script>
 import Card from './Card.vue'
 
-import cardAriana from '../assets/card-ariana.jpg'
-import cardEidy from '../assets/card-eidy.jpg'
 import cardHxh from '../assets/card-hxh.jpg'
+import cardEidy from '../assets/card-eidy.jpg'
+import cardAriana from '../assets/card-ariana.jpg'
 import cardRobots from '../assets/card-robots.jpg'
 
 export default {
   data() {
     return {
-      cardAriana,
-      cardEidy,
       cardHxh,
+      cardEidy,
+      cardAriana,
       cardRobots,
     }
   },
   components: {
     Card,
   },
+
 }
 </script>
 
 <template>
-  <main class="z-10 flex flex-col lg:flex-row justify-center py-8 mx-4 bg-[#16161C]">
+  <main class="z-10 flex flex-col lg:flex-row justify-center py-8 mx-4 lg:mx-0 bg-[#16161C]">
 
     <div class="lg:mr-10">
-      <Card
-        id="two"
-        :img="cardHxh"
-        imgAlt="Hunter x Hunter card" 
-        title="Hunter X Hunter" 
-        tags="design"
-        isWhite="true" 
-      />
-      <Card
-        id="one"
-        :img="cardEidy"
-        imgAlt="Planet card" 
-        title="Planets" 
-        tags="design"
-      />
+      <router-link to="/cards/hxh" id="dois">
+        <Card
+          id="two"
+          :img="cardHxh"
+          imgAlt="Hunter x Hunter card" 
+          title="Hunter X Hunter" 
+          tags="design"
+          isWhite="true" 
+        />
+      </router-link>
+
+      <router-link to="/cards/planet">
+        <Card
+          id="one"
+          :img="cardEidy"
+          imgAlt="Planet card" 
+          title="Planets" 
+          tags="design"
+        />
+      </router-link>
     </div>
 
     <div class="lg:mt-40">
-      <Card
-        id="three"
-        :img="cardAriana"
-        imgAlt="Ariana Grande card" 
-        title="Ariana Grande" 
-        tags="design"
-      />
-      <Card
-        id="four"
-        :img="cardRobots"
-        imgAlt="Robots card" 
-        title="Robots" 
-        tags="design"
-        isWhite="true" 
-      />
+      <router-link to="/cards/ariana">
+        <Card
+          id="three"
+          :img="cardAriana"
+          imgAlt="Ariana Grande card" 
+          title="Ariana Grande" 
+          tags="design"
+        />
+      </router-link>
+
+      <router-link to="/cards/robots">   
+        <Card
+          id="four"
+          :img="cardRobots"
+          imgAlt="Robots card" 
+          title="Robots" 
+          tags="design"
+          isWhite="true"
+        />
+      </router-link>
     </div>
 
   </main>
