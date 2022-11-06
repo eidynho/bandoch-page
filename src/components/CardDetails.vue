daf562ca7303dfa0f6332c4e705580709a882f29
eidy <zviniciuseidy2@gmail.com>
2022-11-06 17:40:58 -0300

    update: remove all canvas image with gooey effects from project

--- src/components/CardDetails.vue
+++ src/components/CardDetails.vue
@@ -1,8 +1,6 @@
 <script>
 import { useRoute } from 'vue-router'
 
-import Scene from "./Gooey/Scene.js";
-
 import cardHxh from '../assets/card-hxh.jpg'
 import cardEidy from '../assets/card-eidy.jpg'
 import cardAriana from '../assets/card-ariana.jpg'
@@ -26,43 +24,18 @@ export default {
 			imageAlt: '',
 		}
 	},
-	// created() {
-	// 	let timer = null
-	// 	window.addEventListener("resize", () => {
-	// 		clearTimeout(timer)
 
-	// 		const canvaEl = document.querySelector('.canva-card-details')
-	// 		canvaEl.style.filter = 'blur(10px)'
-
-
-	// 		timer = setTimeout(function () {
-	// 			window.scene = new Scene()
-	// 			canvaEl.style.filter = 'blur(0px)'
-	// 		}, 400)
-
-	// 	})
-	// },
 	mounted() {
 		const route = useRoute()
 		this.cardName = route.params.name
 		this.getData(this.cardName)
 
+		// on load, return scroll to top of page
 		this.$nextTick(() => {
-			window.scene = new Scene()
-
 			var element = document.getElementById("card-details")
 			window.scrollTo(0, element.offsetTop)
 		}, 0)
-
-		// setTimeout(() => {
-		// 	window.scene = new Scene()
-		// 	document.querySelector('.canva-card-details').style.display = 'block'
-		// }, 2000)
 	},
-
-	// destroyed() {
-	// 	window.removeEventListener("resize", window.scene = new Scene());
-	// },
 
 	methods: {
 		getData(cardName) {
@@ -105,7 +78,8 @@ export default {
 					break
 			}
 		},
-	}
+	},
+
 }
 </script>
   
@@ -128,19 +102,10 @@ export default {
 			</div>
 		</div>
 
+		<!-- principal image -->
 		<div class="z-10 relative mt-12 max-w-xl mx-auto cardDimensions">
 			<img :src="cardImage" :alt="imageAlt" class="w-full h-full rounded-3xl">
 		</div>
-
-		<!-- <section class="container-three">
-			<article class="tile">
-				<figure class="tile__figure">
-					<img :src="cardImage" :data-hover="cardImageHover" class="tile__image" alt="My image" />
-				</figure>
-			</article>
-		</section>
-		<canvas id="stage" class="canva-card-details"></canvas> -->
-
 
 		<footer class="mt-40 w-full flex justify-between">
 			<div class="flex flex-col">
@@ -177,49 +142,4 @@ export default {
 	}
 }
 
-.canva-card-details {
-	transition: all 0.4s ease;
-	display: none;
-}
-
-.container-three {
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	width: 100%;
-	height: 100vh;
-	z-index: 10;
-	position: relative;
-}
-
-.tile {
-	width: 85vmin;
-	position: absolute;
-	top: -24%;
-}
-
-.tile__figure {
-	margin: 0;
-	padding: 0;
-	position: absolute;
-	top: 0;
-	left: 0;
-}
-
-.tile__image {
-	width: 100%;
-	height: 100%;
-	object-fit: cover;
-	object-position: center;
-}
-
-canvas {
-	width: 100%;
-	height: 100%;
-	z-index: 9;
-	position: absolute;
-	top: 38%;
-	left: 4.4%;
-	overflow: visible;
-}
 </style>
